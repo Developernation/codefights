d7d1f0e7e6990f4daaf5351275cef07915bd1943
Sim <simeon.f.anderson@gmail.com>
2018-02-17 19:14:38 +0000

    working with sliding window compression: added better partial solution

--- cfights/python3_solutions/bfights/slidingWindowC.py
+++ cfights/python3_solutions/bfights/slidingWindowC.py
@@ -1,3 +1,4 @@
+#1) Partial solution
 def losslessDataCompression(iString, w):
         if len(iString) < 2:
                 return iString
@@ -43,3 +44,47 @@ def losslessDataCompression(iString, w):
         #startInd = iString.index(win[i],i)
         #win_len = len(win)
         #if iString[i:i + len(iString) - 1] == iString[startInd,startInd + ]
+#2) Better partial solution to find length
+def losslessDataCompression2(iString, width):
+        #----------Variables------------
+        result = []
+        win = []
+        fnd_such_lst = [] #tuples
+        i = 1 #current char index
+        len_sub_win = 0 #this is the length variable you want to use
+        #-------------------------------
+        if len(iString) < 2:
+                return iString
+        if len(list(set(iString))) == len(iString):
+                return iString
+        #-------------------------------
+        while i-1 < len(iString):
+                        
+                if i < width: #if there are less than width chars before the current one
+                        win = iString[0:i-1] #width of chars before the current one
+                        len_win = len(win) #length
+                        stInd = 0
+
+                else:
+                        win = iString[i-width:i-1] #width of chars before the current one
+                        len_win = len(win) #length
+                        stInd = i-width
+
+                print(iString[stInd:stInd + len_win])
+                print(stInd,len_win)
+                #find such startIndex and length that.....
+                if (iString[i:i + len_win-1] == iString[stInd:stInd + len_win-1]) and iString[stInd:stInd + len_win-1] in win:
+                        tupe_to_append = (stInd,len_win)
+                        fnd_such_lst.append(tupe_to_append)               
+                
+                        #---------------------------Eval fnd_such_lst--------------------------------------------
+                        max_len = max([fnd_such_lst[i][1] for i in range(len(fnd_such_lst))]) #gets max len
+                        max_len_tup = [fnd_such_lst[t] for t in range(len(fnd_such_lst)) if fnd_such_lst[t][1] == max_len]
+                        if len(max_len_tup) > 1:
+                                min_st = min([fnd_such_lst[i][0] for i in range(len(fnd_such_lst))]) #gets max len
+                                select_tup = [tup for tup in fnd_such_lst if tup == (min_st,max_len)][0]
+                        else:
+                                select_tup = max_len_tup[0]
+                        result.append(select_tup)
+                        len_sub_win = select_tup[1]
+                i += 1
