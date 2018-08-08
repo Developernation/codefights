68f8559f4132210b2cdaf396b1dd0a8e017d3402
developernation <simeon.f.anderson@gmail.com>
2018-08-08 03:12:18 -0400

    changed color variable in PG_utils

--- PyGameLrn/OOP_PG_racer/PG_utils.py
+++ PyGameLrn/OOP_PG_racer/PG_utils.py
@@ -1,8 +1,9 @@
 import pygame as pg
+from PG_Bkg import PG_Bkg as pgb
 
 #--------------FUNCTIONS-------------------
 def text_objects(txt,font):
-    txtSurface = font.render(txt,True,RED)
+    txtSurface = font.render(txt,True,pgb.colors['RED'])
     return txtSurface, txtSurface.get_rect()
 
 def get_sounds(soundPath):
